4e2a42705180975125ddeadba1b1cbf697fd0432
garry kharoud <kharoudgarry111@gmail.com>
2024-03-16 20:52:32 -0400

    Made ICE 8

---
+++ ICE8/app/src/main/java/ca/georgiancollege/comp3025_w2024_week9/ApiResponse.kt
@@ -0,0 +1,11 @@
+package ca.georgiancollege.comp3025_w2024_week9
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class ApiResponse<T>(
+    @Json(name = "success") val success: Boolean,
+    @Json(name = "msg") val message: String,
+    @Json(name = "data") val data: T
+)
